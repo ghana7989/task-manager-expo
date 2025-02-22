eec44bdaaeeb5f0ad7e887db7e02cf325c62fc0f
Pavan Chindukuri <chindukuripavan@gmail.com>
2025-02-22 21:50:27 +0530

    Update MongoDB connection and auth token configuration

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/task-manager')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
